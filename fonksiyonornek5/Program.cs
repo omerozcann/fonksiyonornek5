8f23a2a6246184d89ce2987bfebb62dd276b0761
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-12-22 11:06:18 +0300

    Proje dosyası ekle.

---
+++ fonksiyonornek5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fonksiyonornek5
+{
+    internal class Program
+    {
+        static void yaz()
+        {
+            for (int i = 0; i <5;i++)
+            {
+                for(int j = 0; j < 5;j++)
+                {
+                    Console.Write("A");
+                }
+                Console.WriteLine("");
+            }
+        }
+        static void Main(string[] args)
+        {
+            /*
+             Örnek: çağırıldığı zaman ekrana aşağıdaki matrisi
+            oluşturacak fonksiyonu yazınız.
+            AAAAA
+            AAAAA
+            AAAAA
+            AAAAA
+            AAAAA
+             */
+            yaz();
+        }
+    }
+}
